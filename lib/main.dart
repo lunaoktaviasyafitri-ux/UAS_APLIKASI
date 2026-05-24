536ee55a5ce9a8d4ab39d0744349ec067a577f59
lunaoktaviasyafitri <lunaoktaviasyafitri@gmail.com>
2026-05-24 09:41:33 +0700

    Commit 13: Menambahkan icon emoji yang kurang

--- lib/main.dart
+++ lib/main.dart
@@ -192,6 +192,7 @@ class VariantSelectionScreen extends StatelessWidget {
                     "🐠",
                     "🦑",
                     "🐡",
+                    "🦀",
                     "🐠",
                     "🦞",
                     "🐡",
