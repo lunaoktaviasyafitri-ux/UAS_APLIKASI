1d39906e73633ab1ba987521bac36329f95dbc7b
lunaoktaviasyafitri <lunaoktaviasyafitri@gmail.com>
2026-05-30 01:36:42 +0700

    commit 16: mengganti nama folder

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.emoji_match"
+    namespace = "com.luna.emoji_match"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.emoji_match"
+        applicationId = "com.luna.emoji_match"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
